bdb5566fa9b24e810fc142d464e64397a10cfd15
sya-ri <sya79lua@gmail.com>
2021-08-14 13:45:02 +0900

    Kotlin のバージョンを 1.5.21 に変更

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 import org.jlleitschuh.gradle.ktlint.KtlintExtension
 
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.5.21"
     id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
     id("com.github.johnrengelman.shadow") version "7.0.0"
     id("net.minecrell.plugin-yml.bukkit") version "0.4.0"
